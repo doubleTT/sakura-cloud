8ab0fd85cb898932d5387c46aaedfe6893b0b58f
colansd <cola_nsd@163.com>
2020-07-17 10:05:11 +0800

    commit

--- sakura-cloud-common/src/main/java/com/cola/sakura/common/exception/GlobalExceptionHandler.java
+++ sakura-cloud-common/src/main/java/com/cola/sakura/common/exception/GlobalExceptionHandler.java
@@ -3,10 +3,8 @@ package com.cola.sakura.common.exception;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import com.cola.sakura.common.enums.ResultEnum;
-import com.cola.sakura.common.result.DataResult;
 import com.cola.sakura.common.result.Result;
 import com.cola.sakura.common.util.DataResultUtil;
-import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import javax.security.auth.message.AuthException;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.Set;
@@ -89,22 +86,6 @@ public class GlobalExceptionHandler {
         log.warn("Bind Exception", e);
         FieldError error = e.getFieldError();
         String message = String.format("%s:%s", error.getField(), error.getDefaultMessage());
-        return DataResultUtil.error(ResultEnum.BAD_REQUEST.getValue(),message);
-    }
-
-    /**
-     * @Description: 参数校验错误异常
-     * @Author cola
-     * @Date 2020/7/16 18:45
-     **/
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseBody
-    public Result handleError(ConstraintViolationException e) {
-        log.warn("Constraint Violation", e);
-        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
-        ConstraintViolation<?> violation = violations.iterator().next();
-        String path = ((PathImpl) violation.getPropertyPath()).getLeafNode().getName();
-        String message = String.format("%s:%s", path, violation.getMessage());
         return DataResultUtil.error(ResultEnum.BAD_REQUEST.getValue(),message);
     }
 
